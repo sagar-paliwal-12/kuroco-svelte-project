e02e9909363e3610744b0aca137ec6a6b15483e5
sagar-paliwal-12 <sagar@diverta.co.jp>
2022-06-08 15:28:23 +0530

    setup base for project

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,49 @@
+<nav>
+    <img src="https://sample-intern-site.g.kuroco-img.app/files/user/logo.png" height="30" alt="">
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/services">Services</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/contact">Contact</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background: #e9af3c;
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+    }
+    img {
+        margin-right: 40px;
+    }
+    ul {
+        display: flex;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    li {
+        width: 100px;
+        letter-spacing: 1px;
+        margin-right: 10px;
+        font-size: 20px;
+        text-decoration-color: #c62828;
+    }
+    li a {
+        color: #c62828;
+        text-decoration: none;
+    }
+    li a:hover {
+        color: #000
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Nav from "$lib/Nav.svelte"
+</script>
+
+<div class="container">
+    <Nav />
+    <slot />
+</div>
+
+<style>
+    .container {
+        height: 100vh;
+    }
+</style>
